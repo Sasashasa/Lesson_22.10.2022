976903a0a8471fbeb3c94af45799bc11dec198e0
Sasashasa <sasashasa321@gmail.com>
2022-10-22 13:48:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson_22102022/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.lesson_22102022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button _button;
+    private TextView _textView;
+    private EditText _editText;
+    private int _counter = 0;
+    private String _name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Нахождение элемента интерфейса в разметке по айди
+        _button = findViewById(R.id.button);
+        _textView = findViewById(R.id.textView);
+        _editText = findViewById(R.id.editTextView);
+
+        // Анонимный внутренний класс
+        _button.setOnClickListener(new View.OnClickListener()
+        {
+            // Метод onClick() вызывается каждый раз при нажатии на кнопку
+            @Override
+            public void onClick(View view)
+            {
+                _counter++;
+                _textView.setText(String.valueOf(_counter));
+                _name = String.valueOf(_editText.getText());
+                Toast.makeText(MainActivity.this, "меня нажал " + _name, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
